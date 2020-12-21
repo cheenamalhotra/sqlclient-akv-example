52b7042fe190f77b789c9f96a60f294b5ff8828c
Cheena Malhotra <v-chmalh@microsoft.com>
2020-12-21 14:36:35 -0800

    Initial Commit

---
+++ Constants.cs
@@ -0,0 +1,18 @@
+﻿public static class Constants
+{
+    public static readonly string s_algorithm = "RSA_OAEP";
+
+    // ********* Provide details here ***********
+    public static readonly string s_akvUrl = "https://{KeyVaultName}.vault.azure.net/keys/{Key}/{KeyIdentifier}";
+    public static readonly string s_clientId = "{Application_Client_ID}";
+    public static readonly string s_clientSecret = "{Application_Client_Secret}";
+
+    /** ********* Provide connection string here, examples: ***********
+     * Server=localhost; Integrated Security=true; Column Encryption Setting=Enabled;
+     * Server={azureserver}.database.windows.net; Authentication= Active Directory MSI; Column Encryption Setting=Enabled;
+     * Server={azureserver}.database.windows.net; Authentication= Active Directory Interactive; Column Encryption Setting=Enabled;"
+     **/
+    public static readonly string s_connectionString = "Server=localhost; Integrated Security=true; Column Encryption Setting=Enabled;";
+    // ******************************************
+
+}
